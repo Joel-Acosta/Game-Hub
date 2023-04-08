432387c855f982868a82099d5776554843777900
joel <tec.joelacosta@gmail.com>
2023-04-08 14:34:47 -0300

    Clean up the game  cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,7 @@ function GameGrid({ gameQuery }: Props) {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding={10} spacing={3}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={10} spacing={6}>
                 {isLoading &&
                     Skeletons.map((Skeleton) => {
                         return (
