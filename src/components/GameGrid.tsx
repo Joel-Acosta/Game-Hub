a139b8270b298a03f6222ddef3855a68e136752a
joel <tec.joelacosta@gmail.com>
2023-04-09 12:22:42 -0300

    Refractor: Gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,9 +15,9 @@ function GameGrid({ gameQuery }: Props) {
     const { data, error, isLoading } = useGames(gameQuery)
     const Skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
+   if (error) return <Text>{error}</Text>
     return (
-        <>
-            {error && <Text>{error}</Text>}
+        
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={10} spacing={6}>
                 {isLoading &&
                     Skeletons.map((Skeleton) => {
@@ -33,7 +33,6 @@ function GameGrid({ gameQuery }: Props) {
                     </GameCardContainer>
                 ))}
             </SimpleGrid>
-        </>
     )
 }
 
